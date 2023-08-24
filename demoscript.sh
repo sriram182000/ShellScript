8bb8771bbedd34aafcc45e5cfed7fea8ebfee18a
Bhamidipalli <AAF7919@carefirst.com>
2023-08-24 17:53:50 +0530

    practice

--- demoscript.sh
+++ demoscript.sh
@@ -5,8 +5,22 @@
 
 num1=$1
 num2=$2
-
+arr=(23 "ram" 31 6.7)
 
 sum=$((num1+num2))
 
 echo $sum
+
+echo ${arr[2]}
+
+read -p "enter the username:" username
+echo "The username is $username"
+echo -ne "enter password:"
+read -s password
+
+echo "the password is: $password"
+
+
+q=`cat demoscript.sh`
+
+echo $q
